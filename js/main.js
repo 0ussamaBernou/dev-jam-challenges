1fd7362f4b5293559d7280a2c4e64207ff6bd663
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-07-08 21:23:01 +0100

    V1.0

--- js/main.js
+++ js/main.js
@@ -23,11 +23,32 @@ fetch("https://randomuser.me/api")
     .then(response => response.json()) // parse response as JSON
     .then(data => {
         console.log(data)
-        let dataList = document.querySelector('.data')
-        let person = data.results[0]
-        let avatar = person.picture.large
-        let fullName = `${person.name.title}.${person.name.first} ${person.name.last}`
+
+        // DATA NEEDED:
+        const person = data.results[0]
+        const avatar = person.picture.large
+        const fullName = `${person.name.title}. ${person.name.first} ${person.name.last}`
         console.log(fullName)
+        const username = `<bold>Username:</bold> ${person.login.username}`
+        const email = `<bold>Email:</bold> ${person.email}`
+        const phone = `<bold>Phone:</bold> ${person.phone}`
+        const location = ` <bold>Location:</bold> ${person.location.country}, ${person.location.city}, ${person.location.street.name}, N°${person.location.street.number}`
+        const infos = [fullName, username, email, phone, location]
+
+        // HTML ELEMENTS
+        const dataList = document.querySelector('#data-list')
+        const img = document.querySelector('.image')
+
+        // SCRIPT
+        img.src = avatar
+        let list = ''
+        for(let i=0; i<infos.length; i++){
+            if(i==0){
+                list += `<li><h1>${infos[i]}</h1></li>`
+            }else
+            list += `<li>${infos[i]}</li>`
+        }
+        dataList.innerHTML = list
 
     })
     .catch(error => {
